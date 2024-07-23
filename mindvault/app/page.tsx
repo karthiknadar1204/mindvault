516509c8fc44208fe1b5212f6a203ef05ec58648
karthik <karthiknadar1204@gmail.com>
2024-07-23 22:38:11 +0530

    testing convex queries

--- mindvault/app/page.tsx
+++ mindvault/app/page.tsx
@@ -1,9 +1,12 @@
 "use client"
 import Image from "next/image";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import { Authenticated, Unauthenticated, useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function Home() {
+  const documents=useQuery(api.documents.getDocuments)
+  const createDocument=useMutation(api.documents.createDocument);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <Unauthenticated>
@@ -11,7 +14,12 @@ export default function Home() {
       </Unauthenticated>
       <Authenticated>
         <UserButton />
-        {/* <Content /> */}
+        <button onClick={()=>{
+          createDocument({title:"Hello world"})
+        }} >Click Me</button>
+        {documents?.map((doc)=>(
+          <div key={doc._id} >{doc.title}</div>
+        ))}
       </Authenticated>
     </main>
   );
---
+++ mindvault/convex/documents.ts
@@ -0,0 +1,31 @@
+import {
+    MutationCtx,
+    QueryCtx,
+    action,
+    internalAction,
+    internalMutation,
+    internalQuery,
+    mutation,
+    query,
+  } from "./_generated/server";
+  import { ConvexError, v } from "convex/values";
+
+
+export const getDocuments=query({
+    async handler(ctx){
+        return await ctx.db.query('documents').collect()
+    }
+})
+
+
+
+export const createDocument = mutation({
+    args: {
+      title: v.string(),
+    },
+    async handler(ctx, args) {
+        await ctx.db.insert('documents',{
+            title:args.title,
+        })
+    },
+  });
